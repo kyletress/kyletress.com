aa52ba2b41953862f07908edd012d82e663f1583
Kyle Tress <kyle@kyletress.com>
2019-08-21 16:57:45 -0400

    maybe this will work

--- config.rb
+++ config.rb
@@ -7,49 +7,10 @@ end
 
 activate :livereload#, host: '138.197.200.236'
 activate :directory_indexes
-activate :google_analytics do |ga|
-  ga.tracking_id = 'UA-1619676-7' # Replace with your property ID.
-end
-# Layouts
-# https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-helpers do
-  def book_link_to(book)
-    if book.link.present?
-      link_to book.title, book.link
-    else
-      book.title
-    end
-  end
-end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
